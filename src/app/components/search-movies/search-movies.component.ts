8d55ae789a6641aafe80d950dcb966e3e0424ca1
Hire1985 <andrezzz85@gmail.com>
2025-12-10 20:28:31 -0500

    Searh working

--- src/app/components/search-movies/search-movies.component.ts
+++ src/app/components/search-movies/search-movies.component.ts
@@ -12,6 +12,6 @@ import { MoviesService } from '../../service/movies.service';
 })
 export default class SearchMoviesComponent {
   moviesService = inject(MoviesService);
-  searchMovies = input.required<Movies[]>()
+  searchMovies = input<Movies[]>()
     imageBaseUrl: string = 'https://image.tmdb.org/t/p/w500';
  }
